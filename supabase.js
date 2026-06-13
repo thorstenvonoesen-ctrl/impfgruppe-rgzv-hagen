508983b78fb4b1b82a22f260ead48bdd6dbcc23c
thorstenvonoesen-ctrl <thorstenvonoesen@googlemail.com>
2026-06-13 12:26:05 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const url = 'https://hwwytaqixoniueaogpsl.supabase.co'
-const anonKey = sb_publishable_dAs5doDB_-aeOm7zhYu5Sg_ot991BnU
+const anonKey = 'sb_publishable_dAs5doDB_-aeOm7zhYu5Sg_ot991BnU'
 
 export const hasSupabase = true
 export const supabase = createClient(url, anonKey)
