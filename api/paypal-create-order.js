0bfaf5c78eb26e260c7b88e8696f53014f2e9c79
thorstenvonoesen-ctrl <thorstenvonoesen@googlemail.com>
2026-06-13 14:43:01 +0200

    Update paypal-create-order.js

--- api/paypal-create-order.js
+++ api/paypal-create-order.js
@@ -1,1 +1,9 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' })
+  }
 
+  res.status(200).json({
+    message: 'PayPal API erreichbar'
+  })
+}
