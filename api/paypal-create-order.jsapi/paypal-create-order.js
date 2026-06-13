cb3c341ff27b01ba16feccb1157357b6fb8de019
thorstenvonoesen-ctrl <thorstenvonoesen@googlemail.com>
2026-06-13 14:38:06 +0200

    Delete api/paypal-create-order.jsapi directory

--- api/paypal-create-order.jsapi/paypal-create-order.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' })
-  }
-
-  res.status(200).json({
-    message: 'PayPal API erreichbar'
-  })
-}
