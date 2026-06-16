009d02f48f92093162bc47bd021fe8b20de25fd7
thorstenvonoesen-ctrl <thorstenvonoesen@googlemail.com>
2026-06-16 09:09:22 +0200

    Update paypal-create-order.js

--- api/paypal-create-order.js
+++ api/paypal-create-order.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
             description: 'Impfgruppe RGZV Hagen',
             amount: {
               currency_code: 'EUR',
-              value: '0,01'
+              value: '1,00'
             }
           }
         ],
