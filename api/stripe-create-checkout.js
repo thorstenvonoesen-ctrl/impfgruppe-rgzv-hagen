208f14281fca53d8c2f8bdfe830fcfc4eded95c5
thorstenvonoesen-ctrl <thorstenvonoesen@googlemail.com>
2026-06-20 18:54:52 +0200

    Update stripe-create-checkout.js

--- api/stripe-create-checkout.js
+++ api/stripe-create-checkout.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
             product_data: {
               name: 'Impfgruppe RGZV Hagen'
             },
-            unit_amount: amount * 50
+            unit_amount: amount * 5
           },
           quantity: 1
         }
