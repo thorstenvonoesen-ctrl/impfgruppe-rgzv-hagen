e7f4236a7777465e3f1d896737be7f944789cb75
thorstenvonoesen-ctrl <thorstenvonoesen@googlemail.com>
2026-07-01 22:02:50 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -209,10 +209,10 @@ function ClubSelect() {
   />
 </div>
           <div style={{textAlign:'left',marginLeft:'70px',alignSelf:'center'}}>
-            <div style={{fontSize:'22px',marginBottom:'34px',color:'#fff'}}>© 2026</div>
-            <div style={{color:'#f28c28',fontSize:'22px',fontFamily:'cursive',marginBottom:'18px'}}>Thorsten von Oesen</div>
-            <div style={{color:'#fff',fontSize:'16px',marginBottom:'18px'}}>Inhaber & Entwickler</div>
-            <div style={{width:'58px',height:'4px',background:'#f28c28',borderRadius:'999px'}} />
+            <div style={{fontSize:'18px',marginBottom:'22px',fontWeight:'500',color:'#fff'}}>© 2026</div>
+            <div style={{color:'#f28c28',fontSize:'20px',fontFamily:'cursive',lineHeight:'1.2',marginBottom:'14px'}}>Thorsten von Oesen</div>
+            <div style={{color:'#fff',fontSize:'14px',lineHeight:'1.4',marginBottom:'16px'}}>Inhaber & Entwickler</div>
+            <div style={{width:'64px',height:'4px',background:'#f28c28',borderRadius:'999px'}} />
           </div>
         </div>
 
