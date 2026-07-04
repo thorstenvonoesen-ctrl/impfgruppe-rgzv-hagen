52a75f5712865c2d7b4041379ef3d82a1f799e8a
thorstenvonoesen-ctrl <thorstenvonoesen@googlemail.com>
2026-07-04 19:43:54 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -83,6 +83,7 @@ const [showForm, setShowForm] = useState(false)
   if (page === '#info') return <InfoPage />
   if (page === '#info-newcastle') return <InfoNewcastle />
   if (page === '#info-pflicht') return <InfoPflicht />
+  if (page === '#info-sammelimpfung') return <InfoSammelimpfung />
 if (page === '#signup') return <PublicSignup />
   if (page === '#admin') return <Admin />
   if (page === '#datenschutz') return <Datenschutz />
