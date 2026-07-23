33ef0021e9e3c38c5204bee0411dc0aed10fd2c1
thorstenvonoesen-ctrl <thorstenvonoesen@googlemail.com>
2026-07-23 13:00:24 +0200

    Update stripe-webhook.js

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -1,1 +1,17 @@
+import Stripe from 'stripe'
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed')
+  }
+
+  res.status(200).send('Webhook erreichbar')
+}
